a57abc641049ebaf604ba5aa91ecc9ce31a95487
Kiss József <vona.2015@gmail.com>
2025-11-11 17:34:21 +0100

    link.tsx frissítés

--- src/components/ui/link.tsx
+++ src/components/ui/link.tsx
@@ -6,16 +6,17 @@
  * https://catalyst.tailwindui.com/docs#client-side-router-integration
  */
 
-import * as Headless from '@headlessui/react'
-import React, { forwardRef } from 'react'
+import * as Headless from "@headlessui/react";
+import NextLink, { type LinkProps } from "next/link";
+import React, { forwardRef } from "react";
 
 export const Link = forwardRef(function Link(
-  props: { href: string } & React.ComponentPropsWithoutRef<'a'>,
+  props: { href: string } & React.ComponentPropsWithoutRef<"a">,
   ref: React.ForwardedRef<HTMLAnchorElement>
 ) {
   return (
     <Headless.DataInteractive>
-      <a {...props} ref={ref} />
+      <NextLink {...props} ref={ref} />
     </Headless.DataInteractive>
-  )
-})
+  );
+});
