d636a0406dd418bdafaf25bc766e6de53a376c31
Kiss József <vona.2015@gmail.com>
2025-11-11 17:39:54 +0100

    tailwind forms telepítés / tailwind.config frissítés a Catalyst miatt

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,16 @@
 // tailwind.config.ts
 import type { Config } from "tailwindcss";
+import "@tailwindcss/forms"; // A Catalyst űrlapoknak szüksége van erre!
 
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}", // <--- FONTOS
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/ui/**/*.{js,ts,jsx,tsx,mdx}", // <--- ÍGY ADD HOZZÁ
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
 };
 export default config;
