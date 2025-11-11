772d217caecf3632f77d18a77b29c03c6b311f75
Kiss József <vona.2015@gmail.com>
2025-11-11 16:30:23 +0100

    Tailwind telepítése

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         />
         <div className={styles.intro}>
           <h1>To get started, edit the page.tsx file.</h1>
-          <p>
+          <p className="text-3xl font-bold underline">
             Looking for a starting point or more instructions? Head over to{" "}
             <a
               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
