5e9425cf2b764e96d8d217b62ec60ee5b8105781
jozsef-kiss <vona.2015@gmail.com>
2025-12-16 16:53:00 +0100

    HomePage/Hero v1.0

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,32 +1,26 @@
 import { getDictionary, Locale } from "@/app/[locale]/dictionaries";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import Image from "next/image"; // Importáltuk a next/image-t
 
 export default async function Hero({ lang }: { lang: Locale }) {
   const dict = await getDictionary(lang);
   const t = dict.HomePage.Hero;
 
   return (
-    // JAVÍTÁS: bg-white -> bg-stone-50
     <div className="bg-stone-50 relative">
-      {/* VÁLTOZÁS: lg: -> xl:
-          A 'xl:flex' miatt csak 1280px felett lesz egymás mellett.
-          Alatta (tehát 1200px-en is) marad a mobil nézet (egymás alatt).
-      */}
       <div className="mx-auto max-w-7xl xl:flex xl:min-h-[calc(100vh-80px)]">
-        <div className="relative z-20 px-6 py-24 sm:py-32 xl:py-0 xl:px-8 xl:w-full xl:max-w-3xl xl:flex xl:flex-col xl:justify-center">
-          {/* A vágás is csak XL mérettől látszik */}
+        <div className="relative z-20 px-6 py-12 sm:py-24 xl:py-0 xl:px-8 xl:w-full xl:max-w-3xl xl:flex xl:flex-col xl:justify-center">
+          {/* A vágás (háromszög) csak XL mérettől látszik */}
           <svg
             viewBox="0 0 100 100"
             preserveAspectRatio="none"
             aria-hidden="true"
-            // JAVÍTÁS: fill-white -> fill-stone-50 (hogy beleolvadjon a háttérbe)
             className="absolute inset-y-0 right-1 hidden h-full w-80 translate-x-1/2 transform fill-stone-50 xl:block pointer-events-none"
           >
             <polygon points="0,0 90,0 50,100 0,100" />
           </svg>
 
-          {/* A szövegdoboz szélesség korlátja is csak XL-en lép életbe */}
           <div className="mx-auto max-w-2xl xl:mx-0 xl:max-w-xl">
             {/* Badge */}
             <div className="hidden sm:mb-10 sm:flex">
@@ -63,16 +57,18 @@ export default async function Hero({ lang }: { lang: Locale }) {
         </div>
       </div>
 
-      {/* KÉP: 
-         - xl:absolute (csak 1280px felett úszik jobbra)
-         - xl:w-1/2 (csak 1280px felett lesz 50%-os)
-         - Alatta sima blokk elemként viselkedik (mobil nézet)
+      {/* A KÉP KONTÉNER:
+         - Alapból (mobil): relative, h-64 (fix magasság), w-full. Így a szöveg ALATT lesz.
+         - XL-től: absolute, inset-y-0, right-0, w-1/2, h-auto. Így beúszik JOBBRA.
       */}
-      <div className="bg-gray-50 xl:absolute xl:inset-y-0 xl:right-0 xl:w-1/2 h-64 xl:h-auto w-full">
-        <img
-          alt="Modern játszótér mászóka gyerekekkel"
-          src="/Hero/hero.png"
-          className="aspect-[3/2] object-cover w-full h-full xl:aspect-auto"
+      <div className="relative h-64 w-full bg-gray-50 xl:absolute xl:inset-y-0 xl:right-0 xl:h-auto xl:w-1/2">
+        <Image
+          alt="Modern Terra Forte Bau fém játszótér komplexum csúszdás toronnyal"
+          src="/images/hero/modern-fem-jatszoter-komplexum-csuszdas-torony-hintaallvany.jpg"
+          fill // Kitölti a konténert
+          className="object-cover" // Méretarányos kitöltés
+          priority // Fontos a betöltési sebesség miatt (LCP)
+          sizes="(max-width: 1280px) 100vw, 50vw" // Mobilon teljes szélesség, asztalin fél
         />
       </div>
     </div>
